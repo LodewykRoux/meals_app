ea0afa7d368f3bd4395f568590b031385d4dce95
lodewyk <lodewyk@thinkninjas.co.za>
2022-06-07 20:34:38 +0200

    added filters and delete button

--- meals/lib/screens/category_meals_screen.dart
+++ meals/lib/screens/category_meals_screen.dart
@@ -1,47 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:meals/dummydata/dummy_data.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/widget/meal_item.dart';
 
-class CategoryMealsScreen extends StatelessWidget {
+class CategoryMealsScreen extends StatefulWidget {
   static const routeName = '/category-meals';
 
   const CategoryMealsScreen({Key? key}) : super(key: key);
-  // final String categoryId;
-  // final String categoryTitle;
-  // const CategoryMealsScreen(
-  //     {Key? key, required this.categoryId, required this.categoryTitle})
-  //     : super(key: key);
+
+  @override
+  State<CategoryMealsScreen> createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  late String categoryTitle;
+  late List<Meal> displayedMeals;
+
+  @override
+  void didChangeDependencies() {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    categoryTitle = routeArgs['title']!;
+    final categoryId = routeArgs['id'];
+    displayedMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+    super.didChangeDependencies();
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((element) => element.id == mealId);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final routeArgs =
-        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
-    final categoryTitle = routeArgs['title'];
-    final categoryId = routeArgs['id'];
-    final categoryMeals = DUMMY_MEALS
-        .where((meal) => meal.categories.contains(categoryId))
-        .toList();
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoryTitle!),
+        title: Text(categoryTitle),
       ),
       body: ListView.builder(
         itemBuilder: (context, index) => MealItem(
-          title: categoryMeals[index].title,
-          imageURL: categoryMeals[index].imageUrl,
-          duration: categoryMeals[index].duration,
-          affordability: categoryMeals[index].affordability,
-          complexity: categoryMeals[index].complexity,
+          title: displayedMeals[index].title,
+          imageURL: displayedMeals[index].imageUrl,
+          duration: displayedMeals[index].duration,
+          affordability: displayedMeals[index].affordability,
+          complexity: displayedMeals[index].complexity,
           selectMeal: () {
-            Navigator.of(context).push(MaterialPageRoute(
+            Navigator.of(context)
+                .push(MaterialPageRoute(
               builder: (context) => MealDetailScreen(
-                meal: categoryMeals[index],
+                meal: displayedMeals[index],
               ),
-            ));
+            ))
+                .then((result) {
+              if (result != null) {
+                _removeMeal(result);
+              }
+            });
           },
         ),
-        itemCount: categoryMeals.length,
+        itemCount: displayedMeals.length,
       ),
     );
     ;
--- meals/lib/screens/filters_screen.dart
+++ meals/lib/screens/filters_screen.dart
@@ -1,14 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:meals/widget/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   const FiltersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Filters')),
       drawer: MainDrawer(),
+      body: Column(children: [
+        Container(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+        ),
+        Expanded(
+            child: ListView(
+          children: [
+            SwitchListTile(
+              value: _glutenFree,
+              onChanged: (_) {
+                setState(() {
+                  _glutenFree = _;
+                });
+              },
+              subtitle: const Text('Only include gluten free meals'),
+              title: const Text('Gluten Free'),
+            ),
+            SwitchListTile(
+              value: _vegetarian,
+              onChanged: (_) {
+                setState(() {
+                  _vegetarian = _;
+                });
+              },
+              subtitle: const Text('Only include vegetarian meals'),
+              title: const Text('Vegetarian'),
+            ),
+            SwitchListTile(
+              value: _vegan,
+              onChanged: (_) {
+                setState(() {
+                  _vegan = _;
+                });
+              },
+              subtitle: const Text('Only include vegan meals'),
+              title: const Text('Vegan'),
+            ),
+            SwitchListTile(
+              value: _lactoseFree,
+              onChanged: (_) {
+                setState(() {
+                  _lactoseFree = _;
+                });
+              },
+              subtitle: const Text('Only include lactose free meals'),
+              title: const Text('Lactose Free'),
+            ),
+          ],
+        ))
+      ]),
     );
   }
 }
